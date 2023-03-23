9b217857757641bcf73554c9a89e6b796c60d9d4
Sergey <s.kuznetsov@scriptheads.io>
2023-03-23 22:17:29 +0300

    Replacing get with XPATH

---
+++ cypress/e2e/Authorization.cy.ts
@@ -0,0 +1,28 @@
+describe('AuthorizationISMART', () => {
+  it('AuthorizationISMART', () => {
+    cy.viewport(1920, 1080)
+    cy.visit('https://canary.ismart.org/')
+    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
+    cy.xpath('//p[contains(text(),"Войти")]').click()
+    cy.xpath('//input[@name="login"]').type('Parent@ismart.org')
+    cy.xpath('//input[@name="password"]').type('123456')
+    cy.xpath('//button[contains(text(),"Войти")]').click()
+    cy.xpath('//div[@class="ismart-mj4ckr-Flexbox-container"]').click()
+    cy.contains('Test1').should('be.visible')
+    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
+    cy.get('.ismart-6zuwx-Flexbox-container').contains('Выйти').click()
+
+    // Авторизация за ученика
+
+    cy.visit('https://canary.ismart.org/')
+    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
+    cy.xpath('//p[contains(text(),"Войти")]').click()
+    cy.xpath('//input[@name="login"]').type('Parent@ismart.org')
+    cy.xpath('//input[@name="password"]').type('123456')
+    cy.xpath('//button[contains(text(),"Войти")]').click()
+    cy.xpath('//h1[contains(text(),"Вася Пуговкин")]').click()
+    cy.contains('Вася Пуговкин').should('be.visible')
+    cy.get('.ismart-1p15wet-Flexbox-container').click()
+    cy.get('.ismart-1gmbgdm-Paragraph-container').contains('Выйти').click()
+  })
+})
