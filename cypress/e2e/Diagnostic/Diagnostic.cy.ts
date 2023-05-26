cf708ccfbfd343a433267d4eabed63764cf801c1
gibithai <dougcarlin@tut.by>
2023-05-26 12:49:48 +0300

    ID instead of xpath

--- cypress/e2e/Diagnostic/Diagnostic.cy.ts
+++ cypress/e2e/Diagnostic/Diagnostic.cy.ts
@@ -3,7 +3,7 @@ describe('guest diagnostic', () => {
     cy.visit('https://canary.ismart.org/diagnostics')
     cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
     cy.visit('https://canary.ismart.org/diagnostics/create')
-    cy.xpath('//div[@class="ismart-yhm5lg-Flexbox-container"]//div[1]')
+    cy.xpath('//div[@class="ismart-17nvw1w-Block-container"]').click()
     cy.wait(2000)
     cy.xpath(
       '//body/div[@id="app"]/div[@class="ismart-r38pru-ScrollView-wrapper"]/div[@class="ismart-1xtyevz-ScrollView-container"]/div[@class="ismart-zz75lc-ScrollView-content"]/div[@class="ismart-4o5ltj-Flexbox-container"]/div[@class="ismart-ha16e2-Flexbox-container"]/div[@class="ismart-137t2zj-Flexbox-container"]/div[@class="ismart-edhpy-Flexbox-container"]/div[@class="ismart-14t7r2j-Flexbox-container"]/div[@class="ismart-yhm5lg-Flexbox-container"]/div[1]'
@@ -11,15 +11,15 @@ describe('guest diagnostic', () => {
     cy.xpath('//div[@class="ismart-yhm5lg-Flexbox-container"]//div[1]').click()
     cy.xpath('//div[@class="ismart-1tzrjz2-Flexbox-container"]').click()
     cy.xpath('//button[contains(text(),"Начать")]').click()
-    for (let i = 0; i <= 15; i++) {
+    for (let i = 0; i <= 14; i++) {
       cy.wait(2000)
       if (Math.round(Math.random()) === 1) {
-        cy.xpath(
-          '//span[@class="ismart-13jccua-Icon-container"]//*[name()="svg"]'
-        ).click()
+        cy.get('#dev-menu__button_right').click()
       } else {
-        cy.xpath('//span[@class="ismart-2epqce-Icon-container"]//*[name()="svg"]').click()
+        cy.get('#dev-menu__button_wrong').click()
       }
     }
   })
 })
+/// /*[name()="svg"]
+/// /*[name()="svg"]
