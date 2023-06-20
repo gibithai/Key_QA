237e824ffb8888966fb10ef1287d5f103a3f9e57
Sergey <s.kuznetsov@scriptheads.io>
2023-06-20 12:27:14 +0300

    fix

--- cypress/e2e/Teacher/Create_class.cy.ts
+++ cypress/e2e/Teacher/Create_class.cy.ts
@@ -8,7 +8,40 @@ describe('Create a class', () => {
     cy.xpath('//input[@name="password"]').type('123456')
     cy.xpath('//button[contains(text(),"Войти")]').click()
     cy.xpath('//span[contains(text(),"Кабинет учителя")]').click()
-    cy.wait(15000)
+    cy.xpath('//button[contains(text(),"Управление классами")]').click()
     cy.xpath('//button[contains(text(),"Новый класс")]').click()
+    cy.xpath('//div[@class="ismart-1yej63f-Select-selectedArea"]').click()
+    cy.xpath('//div[normalize-space()="1"]').click()
+
+    // Генерируем случайную букву английского алфавита
+    const randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26))
+
+    // Вводим случайную букву в поле ввода
+    cy.xpath('//input[@placeholder="Например, Б"]').clear().type(randomLetter)
+    cy.xpath('//button[@class="ismart-ohzuu7-Button-container"]').click()
+    cy.wait(2000)
+    cy.xpath(
+      '//div[@class="ismart-18ha755-Flexbox-container"]//div[@class="ismart-1wgs5pq-Block-container"]'
+    )
+      .trigger('mouseover', { force: true })
+      .click()
+    cy.xpath('//p[contains(text(),"Редактировать")]').click()
+    cy.xpath('//div[@class="ismart-1yej63f-Select-selectedArea"]').click()
+    cy.xpath('//div[normalize-space()="2"]').click()
+    cy.xpath('//button[contains(text(),"Обновить")]').click()
+    /* cy.xpath('//button[contains(text(),"Добавить ученика")]').click()
+    const randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26))
+
+    // Вводим случайную букву в поле ввода
+    cy.xpath('//textarea[@placeholder="Каждый ученик с новой строки"]')
+      .clear()
+      .type(randomLetter)
+    cy.xpath('//button[contains(text(),"Сохранить")]').click()
+    cy.wait(1000)
+    cy.xpath('//div[13]//div[1]//button[1]').click()
+    cy.xpath('//p[contains(text(),"Редактировать")]').click()
+    cy.xpath('//input[@placeholder="Фамилия и Имя"]').clear().type('Test1')
+    cy.xpath('//button[@class="ismart-q760xa-Button-container"]').click()
+    cy.contains('Test1').should('be.visible') */
   })
 })
