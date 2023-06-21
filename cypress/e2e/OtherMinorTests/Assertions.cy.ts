2125bbcd6b6e831ddd23527bf3a9e45bb5c15822
gibithai <dougcarlin@tut.by>
2023-06-21 13:33:52 +0300

    fix

--- cypress/e2e/OtherMinorTests/Assertions.cy.ts
+++ cypress/e2e/OtherMinorTests/Assertions.cy.ts
@@ -1,7 +1,7 @@
 describe('Assertions', () => {
   it('Assertions', () => {
-    cy.visit('https://canary.ismart.org/auth')
-    // should and
+    cy.visit(Cypress.env('BASE_URL') + '/auth')
+    // should and should and
     // cy.url().should("include", 'https://canary.ismart.org')
     // cy.url().should('eq','https://canary.ismart.org/auth')
     // cy.url().should('contain', 'auth')
@@ -9,14 +9,16 @@ describe('Assertions', () => {
         .should('eq','https://canary.ismart.org/auth')
         .should('contain', 'auth') */
 
-    /* cy.url().should("include", 'https://canary.ismart.org')
-        .and('eq','https://canary.ismart.org/auth')
-        .and('contain', 'auth') */
     cy.url()
       .should('include', 'https://canary.ismart.org')
       .and('eq', 'https://canary.ismart.org/auth')
       .and('contain', 'auth')
       .and('not.contain', 'key')
+
+    cy.title()
+      .should('include', 'Вход — iSmart')
+      .and('eq', 'Вход — iSmart')
+      .and('contain', 'Вход — iSmart')
 
     cy.title()
       .should('include', 'Вход — iSmart')
