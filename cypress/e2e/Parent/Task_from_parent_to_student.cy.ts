6a163ea1f0d3de52c6e5a659f0e28fb8607719db
Sergey <s.kuznetsov@scriptheads.io>
2023-06-22 11:41:49 +0300

    fix Task_from_parent_to_student

--- cypress/e2e/Parent/Task_from_parent_to_student.cy.ts
+++ cypress/e2e/Parent/Task_from_parent_to_student.cy.ts
@@ -1,16 +1,16 @@
 describe('Task from parent to student', () => {
+  const userEmail = 'Parent@ismart.org'
+  const userPassword = '123456'
+
   it('Task from parent to student', () => {
     cy.viewport(1920, 1080)
-    cy.visit('https://canary.ismart.org/')
-    // После авторизации выбрать профиль ученика
-    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
-    cy.xpath('//p[contains(text(),"Войти")]').click()
-    cy.xpath('//input[@name="login"]').type('Parent@ismart.org')
-    cy.xpath('//input[@name="password"]').type('123456')
+    cy.visit(String(Cypress.env('BASE_URL')) + 'auth')
+    cy.xpath('//input[@name="login"]').type(userEmail)
+    cy.xpath('//input[@name="password"]').type(userPassword)
     cy.xpath('//button[contains(text(),"Войти")]').click()
     cy.xpath('//h1[normalize-space()="Test1"]').click()
     cy.xpath('//span[contains(text(),"Кабинет родителя")]').click()
-    cy.xpath('//button[@class="ismart-19yjot2-Button-container"]').click()
+    cy.xpath('//button[@class="ismart-5akzmu-Button-container"]').click()
     cy.xpath('//span[@class="ismart-1twvswa"]').type(' Test1')
     cy.xpath('//h4[contains(text(),"Математика (Расширенная версия)")]').click()
     cy.xpath(
