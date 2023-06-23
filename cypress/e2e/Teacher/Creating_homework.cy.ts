a059af1fb927e3f68e66d918bfae14248a9977d8
Sergey <s.kuznetsov@scriptheads.io>
2023-06-23 12:26:29 +0300

    Fix Creating homework

--- cypress/e2e/Teacher/Creating_homework.cy.ts
+++ cypress/e2e/Teacher/Creating_homework.cy.ts
@@ -4,6 +4,7 @@ describe('Creating homework', () => {
   const currentDate = new Date().getDate().toString()
 
   it('Creating homework', () => {
+    // Для всего класса + с датой начала != текущему дню
     cy.viewport(1920, 1080)
     cy.visit(String(Cypress.env('BASE_URL')) + 'auth')
     cy.xpath('//input[@name="login"]').type(userEmail)
@@ -19,6 +20,33 @@ describe('Creating homework', () => {
     ).click()
     cy.xpath(
       '//p[contains(text(),"Пространственные представления: наверху - внизу")]'
+    ).click()
+    cy.xpath('//button[@class="ismart-16zkzwl-Button-container"]').click()
+    cy.contains(currentDate).should('exist')
+    // Для одного ученика + с датой окончания != текущему дню
+    cy.xpath('//button[contains(text(),"Создать занятие")]').click()
+    cy.xpath('//button[@class="ismart-omlitb-Button-container"]').click()
+    cy.xpath('//button[@class="ismart-1m7kcmj-Button-container"]').click()
+    cy.xpath('//h4[contains(text(),"Математика (Расширенная версия)")]').click()
+    cy.xpath(
+      '//a[contains(text(),"Пространственные представления: наверху - внизу")]'
+    ).click()
+    cy.xpath(
+      '//p[contains(text(),"Пространственные представления: наверху - внизу")]'
+    ).click()
+    cy.xpath(
+      '//button[@class="ismart-3qmtd6-Button-container"]//span[@class="ismart-1pqq9jl-Icon-container"]//*[name()="svg"]'
+    ).click()
+    cy.xpath(
+      '//div[@class="ismart-wq0rxi-Flexbox-container"]//div[2]//div[2]//div[1]//div[1]//div[1]//div[1]//div[1]//input[1]'
+    ).click()
+    cy.xpath('//button[contains(text(),"Сегодня")]').should('be.disabled')
+    cy.xpath('//span[contains(text(),"16 учеников")]').click()
+    cy.xpath(
+      '//div[@class="ismart-1yrf1jo-Flexbox-container"]//span[@class="ismart-w1blmz-Icon-container-Checkbox-icon"]//*[name()="svg"]'
+    ).click()
+    cy.xpath(
+      '//div[@class="ismart-j3qx0u-ScrollView-content"]//div[1]//div[1]//div[1]//span[1]//*[name()="svg"]'
     ).click()
     cy.xpath('//button[@class="ismart-16zkzwl-Button-container"]').click()
     cy.contains(currentDate).should('exist')
