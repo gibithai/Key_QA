6673306110b37d7db37aa63d57385207f9d9bb94
gibithai <dougcarlin@tut.by>
2023-06-28 11:57:29 +0300

    catalog test

---
+++ cypress/e2e/Catalog/catalog.cy.ts
@@ -0,0 +1,8 @@
+describe('catalog functionallity', () => {
+  it('catalog tests', () => {
+    cy.visit(String(Cypress.env('BASE_URL')))
+    cy.xpath('//span[contains(text(),"C чего начать")]').click()
+    cy.xpath('//div[@class="ismart-1pyk4il-Flexbox-container"]//div[1]').click()
+    cy.xpath('//div[@class="ismart-wmfrhu-Flexbox-container"]').click()
+  })
+})
