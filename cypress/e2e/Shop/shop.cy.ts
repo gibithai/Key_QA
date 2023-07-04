00c53e09dca85116ed3bfb289ee3dcda10651664
gibithai <dougcarlin@tut.by>
2023-07-04 10:51:41 +0300

    shop test

--- cypress/e2e/Shop/shop.cy.ts
+++ cypress/e2e/Shop/shop.cy.ts
@@ -6,7 +6,8 @@ describe('shop test', () => {
   it('shop test', () => {
     cy.visit(String(Cypress.env('BASE_URL')))
     cy.viewport(1920, 1080)
-    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
+    cy.xpath('//div[@class="ismart-i3f51a-Flexbox-container"]').click()
+    // cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
     cy.xpath('//p[contains(text(),"Войти")]').click()
     cy.xpath('//input[@name="login"]').type('dougcarlin@tut.by')
     cy.xpath('//input[@name="password"]').type('123456')
@@ -28,10 +29,38 @@ describe('shop test', () => {
       '//span[@class="ismart-3nfsk1-Icon-container"]//*[name()="svg"]//*[name()="path" and contains(@clip-rule,"evenodd")]'
     ).click()
     cy.xpath('//button[@class="ismart-1kxq8zk-Button-container"]').click()
+    cy.wait(3000)
     cy.get('iframe')
-      .eq(2)
-      .then(($iframe) => {
-        const $body = $iframe.contents().find('body')
+      .eq(0)
+      .then((iframe) => {
+        const body = iframe.contents().find('body')
+        const cvc = body.find('[automation-id="tui-input-card-grouped__cvc"]')
+        cvc.val('232')
+        cy.wrap('iframe')
       })
+    // cy.get('[automation-id="tui-input-card-grouped__cvc"]').eq(0).click().type('789') НЕ видит!!! Искать этот элемент
+
+    // const el = paycheck.find('[automation-id="tui-input-card-grouped__cvc"]') // .eq(0).click().type('789')
+    // console.log(el)
+    // .get('[automation-id="tui-input-card-grouped__cvc"]')
+    // .eq(0)
+    //
+    // alert('Hello Eugene!')
+    // const body = iframe.contents().find('body')
+    // const cvc = body.find('[automation-id="tui-input-card-grouped__cvc"]')
+    // //
+    // body.within(() => {
+    //   alert('Yo!')
+    // })
+    // const button = body.find('[automation-id="card-form__submit"]')
+    // button.click()
+    // cvc.dispatchEvent(new KeyboardEvent('keydown', { key: '4' }))
+    // cvc.trigger($.Event('keydown', { keyCode: '4' }))
+    // cvc.trigger(jQuery.Event('keydown', { keyCode: '4' }))
+    // window.addEventListener('touchstart', {})
+    // cy.get('iframe')
+    // cy.xpath('//div[@class="ismart-1935w9c-Modal-window"]').click()
+    // })
+    // cy.xpath('//iframe//input[@automation-id="tui-input-card-grouped__cvc"]').type('789')
   })
 })
