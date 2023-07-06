88d95851c964293c1dfa09919062162c6d5c3b18
gibithai <dougcarlin@tut.by>
2023-07-06 15:06:41 +0300

    comments to test shop

--- cypress/e2e/Shop/shop.cy.ts
+++ cypress/e2e/Shop/shop.cy.ts
@@ -7,9 +7,11 @@ describe('shop test', () => {
     cy.xpath('//input[@name="login"]').type('dougcarlin@tut.by')
     cy.xpath('//input[@name="password"]').type('123456')
     cy.xpath('//button[contains(text(),"Войти")]').click()
+    // Авторизация в аккаунт с нужными кредеами
     cy.xpath('//div[@class="ismart-mj4ckr-Flexbox-container"]').click()
     cy.visit(String(Cypress.env('BASE_URL')) + 'shop')
     cy.wait(1000)
+    // Переход в магазин
     cy.xpath(
       '//div[@class="ismart-milwcx-Block-container"]//div[1]//div[1]//div[1]//span[1]//*[name()="svg"]'
     ).click()
@@ -17,6 +19,7 @@ describe('shop test', () => {
     cy.xpath(
       '//div[@class="ismart-klwhgf-Block-container"]//div[2]//div[1]//div[2]//div[3]//button[2]'
     ).click()
+    // Фильтры по предметам в магизине (радио кнопки)
     cy.xpath(
       '//div[@class="ismart-4g250r-Flexbox-container"]//div[3]//div[1]//div[1]//div[3]//button[2]'
     ).click()
@@ -24,6 +27,7 @@ describe('shop test', () => {
       '//span[@class="ismart-3nfsk1-Icon-container"]//*[name()="svg"]//*[name()="path" and contains(@clip-rule,"evenodd")]'
     ).click()
     cy.xpath('//button[@class="ismart-1kxq8zk-Button-container"]').click()
+    // Переход в корзину с товарами
     cy.wait(3000)
     cy.get('iframe')
       .eq(0)
@@ -34,6 +38,7 @@ describe('shop test', () => {
         cy.wait(2000)
         const payButton = body.find('[automation-id="card-form__submit"]')
         cy.wrap(payButton).click()
+        // Заполнение карточной формы (данные дебитовой карты и клик на оплату)
       })
   })
 })
