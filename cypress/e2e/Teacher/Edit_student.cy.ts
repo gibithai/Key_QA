46f3e259b25fac6895b468af36f1ef307b6cdea4
gibithai <dougcarlin@tut.by>
2023-07-06 16:09:57 +0300

    adding comments to all rest tests

--- cypress/e2e/Teacher/Edit_student.cy.ts
+++ cypress/e2e/Teacher/Edit_student.cy.ts
@@ -4,6 +4,7 @@ describe('Edit student', () => {
   const newStudentName = 'Имя изменено'
   it('Edit student', () => {
     cy.viewport(1920, 1080)
+    // Авторизация в аккаунт
     cy.visit(String(Cypress.env('BASE_URL')) + 'auth')
     cy.xpath('//input[@name="login"]').type(userEmail)
     cy.xpath('//input[@name="password"]').type(userPassword)
@@ -13,6 +14,7 @@ describe('Edit student', () => {
     cy.xpath(
       '//div[@class="ismart-aniv23-Flexbox-container"]//div[3]//div[1]//button[1]//span[1]//*[name()="svg"]'
     ).click()
+    // Редактируем имя и фамилию
     cy.xpath('//p[contains(text(),"Редактировать")]').click()
     cy.xpath('//input[@placeholder="Фамилия и Имя"]').clear().type(newStudentName)
     cy.xpath('//span[@class="ismart-1wt7gtz-Icon-container"]//*[name()="svg"]').click()
