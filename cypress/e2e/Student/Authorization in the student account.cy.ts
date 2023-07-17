623708005eae75945ac4e6be2b2e07b0ccbdd56e
Sergey <s.kuznetsov@scriptheads.io>
2023-07-17 21:56:29 +0300

    student auth

--- cypress/e2e/Student/Authorization in the student account.cy.ts
+++ cypress/e2e/Student/Authorization in the student account.cy.ts
@@ -1,12 +1,12 @@
-describe('Authorization in the student account', () => {
+describe('Авторизация в аккаунте ученика', () => {
   const parentEmail = 'parent@ismart.org'
   const parentPassword = '123456'
 
-  it('  Authorization in the student account', () => {
+  it('должен войти в учетную запись ученика и проверить профиль ученика', () => {
     cy.viewport(1920, 1080)
     cy.visit(Cypress.env('BASE_URL'))
-    // После авторизации выбрать профиль ученика
-    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
+    // После авторизации выберите профиль ученика
+    cy.xpath('//div[@class="ismart-5mymc9-Flexbox-container"]').click()
     cy.xpath('//p[contains(text(),"Войти")]').click()
     cy.xpath('//input[@name="login"]').type(parentEmail)
     cy.xpath('//input[@name="password"]').type(parentPassword)
@@ -15,12 +15,9 @@ describe('Authorization in the student account', () => {
     cy.xpath(
       '//body/div[@id="app"]/div[@class="ismart-r38pru-ScrollView-wrapper"]/div[@class="ismart-1xtyevz-ScrollView-container"]/div[@class="ismart-zz75lc-ScrollView-content"]/div[@class="ismart-4o5ltj-Flexbox-container"]/div[@class="ismart-1xt4q15-Flexbox-container"]/div[@class="ismart-z951zb-Flexbox-container"]/form[@class="ismart-m9p5u1"]/div[@class="ismart-v8c4be-Flexbox-container"]/div[@class="ismart-5gneco-Grid-container"]/div[2]'
     ).click()
-<<<<<<< HEAD:cypress/e2e/Student/Authorization in the student account.cy.ts
-=======
     cy.xpath('//p[@class="ismart-1pjcu8j-Paragraph-container"]')
       .invoke('text')
       .should('eq', 'Вася Пуговкин')
->>>>>>> other:cypress/e2e/Student/Student_auth.cy.ts
     // Переход в аккаунт родителя
     cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
     cy.xpath('//p[contains(text(),"Родительский аккаунт")]').click()
@@ -28,17 +25,36 @@ describe('Authorization in the student account', () => {
       .invoke('text')
       .should('eq', 'Test1')
 
-    // Через меню родителя
-    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
-    cy.xpath(
-      '//body/div[@class="ismart-rtrrel-Drop-container"]/div[@class="ismart-r6rs4d-Flexbox-container"]/div[@class="ismart-16e4mz2-ScrollView-wrapper"]/div[@class="ismart-1xtyevz-ScrollView-container"]/div[@class="ismart-7t7otd-ScrollView-content"]/div[1]'
-    ).click()
+    // Проверка имени профиля ученика
+    cy.xpath('//h1[contains(text(),"Вася Пуговкин")]')
+      .invoke('text')
+      .should('eq', 'Вася Пуговкин')
+  })
+
+  it('переключение через меню родителя', () => {
+    cy.visit(Cypress.env('BASE_URL'))
+    cy.xpath('//div[@class="ismart-5mymc9-Flexbox-container"]').click()
+    cy.xpath('//p[contains(text(),"Войти")]').click()
+    cy.xpath('//input[@name="login"]').type(parentEmail)
+    cy.xpath('//input[@name="password"]').type(parentPassword)
+    cy.xpath('//button[contains(text(),"Войти")]').click()
+    cy.xpath('//h1[normalize-space()="Test1"]').click()
+    cy.xpath('//div[@class="ismart-5mymc9-Flexbox-container"]').click()
+    cy.xpath('//p[contains(text(),"Вася Пуговкин")]').click()
     cy.xpath('//p[@class="ismart-1pjcu8j-Paragraph-container"]')
       .invoke('text')
       .should('eq', 'Вася Пуговкин')
-    cy.xpath('//div[@class="ismart-1p15wet-Flexbox-container"]').click()
-    cy.xpath('//p[contains(text(),"Родительский аккаунт")]').click()
-    // Через Кабинет родителя
+  })
+
+  it('переключение через профиль родителя', () => {
+    cy.visit(Cypress.env('BASE_URL'))
+    cy.xpath('//div[@class="ismart-5mymc9-Flexbox-container"]').click()
+    cy.xpath('//p[contains(text(),"Войти")]').click()
+    cy.xpath('//input[@name="login"]').type(parentEmail)
+    cy.xpath('//input[@name="password"]').type(parentPassword)
+    cy.xpath('//button[contains(text(),"Войти")]').click()
+    cy.xpath('//h1[normalize-space()="Test1"]').click()
+    cy.xpath('//div[@class="ismart-5mymc9-Flexbox-container"]').click()
     cy.xpath('//span[contains(text(),"Кабинет родителя")]').click()
     cy.xpath('//button[contains(text(),"Войти как ученик")]').click()
     cy.xpath('//p[@class="ismart-1pjcu8j-Paragraph-container"]')
