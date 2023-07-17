c27c072bdc7286c4ccc1948a5cdb865f116301c5
gibithai <dougcarlin@tut.by>
2023-07-17 09:22:33 +0300

    stage changer st ahop test

--- cypress/e2e/Shop/shop.cy.ts
+++ cypress/e2e/Shop/shop.cy.ts
@@ -1,5 +1,6 @@
 describe('shop test', () => {
   it('shop test', () => {
+    // Авторизация в аккаунт с нужными кредами
     cy.visit(String(Cypress.env('BASE_URL')))
     cy.xpath('//div[@class="ismart-i3f51a-Flexbox-container"]').click()
     cy.xpath('//p[contains(text(),"Войти")]').click()
@@ -7,8 +8,10 @@ describe('shop test', () => {
     cy.xpath('//input[@name="password"]').type('123456')
     cy.xpath('//button[contains(text(),"Войти")]').click()
     cy.xpath('//div[@class="ismart-mj4ckr-Flexbox-container"]').click()
+    // Переход в магазин
     cy.visit(String(Cypress.env('BASE_URL')) + 'shop')
     cy.wait(2000 as number)
+    // Фильтры по предметам в магизине (радио кнопки)
     cy.xpath(
       '//div[@class="ismart-milwcx-Block-container"]//div[1]//div[1]//div[1]//span[1]//*[name()="svg"]'
     ).click()
@@ -16,23 +19,26 @@ describe('shop test', () => {
     cy.xpath(
       '//div[@class="ismart-klwhgf-Block-container"]//div[2]//div[1]//div[2]//div[3]//button[2]'
     ).click()
+    // Переход в корзину с товарами
     cy.xpath(
-      '//div[@class="ismart-4g250r-Flexbox-container"]//div[3]//div[1]//div[1]//div[3]//button[2]'
+      '//div[@class="ismart-klwhgf-Block-container"]//div[2]//div[1]//div[2]//div[3]//button[2]'
     ).click()
     cy.xpath(
-      '//span[@class="ismart-3nfsk1-Icon-container"]//*[name()="svg"]//*[name()="path" and contains(@clip-rule,"evenodd")]'
+      '//div[@class="ismart-klwhgf-Block-container"]//div[2]//div[1]//div[2]//div[3]//button[2]'
     ).click()
     cy.xpath('//button[@class="ismart-1kxq8zk-Button-container"]').click()
-    cy.wait(2000 as number)
+
+    cy.wait(3000 as number)
     cy.get('iframe')
       .eq(0)
       .then((iframe) => {
         const body = iframe.contents().find('body')
         const cvc = body.find('[automation-id="tui-input-card-grouped__cvc"]')
         cy.wrap(cvc).focus().type('123')
-        cy.wait(2000 as number)
+        cy.wait(2000)
         const payButton = body.find('[automation-id="card-form__submit"]')
         cy.wrap(payButton).click()
+        // Заполнение карточной формы (данные по карте и клик на оплату)
       })
   })
 })
