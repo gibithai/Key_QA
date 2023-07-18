fa0c9e6b96f46a56e94cf2445620cd63b6130543
gibithai <dougcarlin@tut.by>
2023-07-18 12:16:33 +0300

    shop 2 total fix

--- cypress/e2e/Shop/shop_deleting_filter_visibility.cy.ts
+++ cypress/e2e/Shop/shop_deleting_filter_visibility.cy.ts
@@ -1,5 +1,5 @@
 describe('shop test', () => {
-  it('shop test', () => {
+  it('Фильтры, удаление и редактирование корзины', () => {
     // Авторизация в аккаунт с нужными кредами
     cy.visit(String(Cypress.env('BASE_URL')))
     cy.xpath('//div[@class="ismart-i3f51a-Flexbox-container"]').click()
@@ -26,16 +26,24 @@ describe('shop test', () => {
     cy.xpath('//div[4]//div[1]//div[2]//div[3]//button[2]').click()
     cy.xpath('//span[@class="ismart-3nfsk1-Icon-container"]//*[name()="svg"]').click()
 
-    // Проверка выбранных позиций в корзине
-    cy.xpath('//div[@class="ismart-1ur7znn-Modal-wrapper"]').should('have.length', 1)
-    cy.contains('Математика ОГЭ').should('be.visible')
-    cy.contains('Русский язык ОГЭ').should('be.visible')
-    cy.contains('Контрольные по математики').should('be.visible')
+    // Проверка, что заголовок страницы содержит 'Магазин — iSmart'
+    cy.title().should('include', 'Магазин — iSmart')
 
-    // Проверка выбранных позиций в корзине
-    cy.xpath('//div[@class="ismart-1ur7znn-Modal-wrapper"]').should('have.length', 1)
-    cy.contains('Математика ОГЭ').should('be.visible')
-    cy.contains('Русский язык ОГЭ').should('be.visible')
-    cy.contains('Контрольные по математики').should('be.visible')
+    // Проверка, что содержимое корзины соответствует выбранным позициям
+    cy.contains('Русский язык ОГЭ').should('have.text', 'Русский язык ОГЭ')
+    cy.xpath(
+      '//span[@class="ismart-1qc69st-Text-container"][contains(text(),"300 ₽")]'
+    ).should('have.text', '300 ₽')
+    cy.xpath(
+      '//span[@class="ismart-fl2eb0-Text-container"][contains(text(),"1 800 ₽")]'
+    ).should('have.text', '1 800 ₽')
+    cy.contains('Русский язык ЕГЭ').should('contain', 'Русский язык ЕГЭ')
+
+    // Проверка отображения суммы Итого
+    cy.xpath('//span[@class="ismart-kb87dw-Text-container"]').should('be.visible')
+    // Удаление выбранных позиций из корзины
+    cy.xpath('//span[@class="ismart-1yjihfb-Icon-container"]//*[name()="svg"]').click()
+    cy.xpath('(//div[@class="ismart-17an9po-Flexbox-container"])[3]').click()
+    cy.xpath('//button[contains(text(),"Удалить (1)")]').click()
   })
 })
